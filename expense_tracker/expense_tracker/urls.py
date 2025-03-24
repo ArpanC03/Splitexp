4e521f128ddc6f250b516301e975eb3de385a2ba
Arpan Chatterjee <126341173+ArpanC03@users.noreply.github.com>
2025-03-24 19:20:00 +0530

    Delete expense_tracker directory

--- expense_tracker/expense_tracker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('expenses.urls')),  # Include expenses app URLs under /api/
-]
--- expense_tracker/expenses/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ExpenseListView
-
-urlpatterns = [
-    path('expenses/', ExpenseListView.as_view(), name='expense-list'),
-]
