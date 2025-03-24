880f9165076a4880b5205d76aada32984b795c4a
Arpan Chatterjee <126341173+ArpanC03@users.noreply.github.com>
2025-03-24 20:04:44 +0530

    Add files via upload

---
+++ expense/backend/CostApp/src/test/java/com/cost/CostAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
