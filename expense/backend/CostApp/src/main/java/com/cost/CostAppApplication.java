9ae022135dc3a66358c4c3567cb6d43d474aed09
Arpan Chatterjee <126341173+ArpanC03@users.noreply.github.com>
2025-03-24 19:55:29 +0530

    Add files via upload

---
+++ expense/backend/CostApp/src/main/java/com/cost/CostAppApplication.java
@@ -0,0 +1,13 @@
+package com.cost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostAppApplication.class, args);
+	}
+
+}
